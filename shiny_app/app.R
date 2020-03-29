74c6dd6d22460e2842a5c4e677de43225d269474
llendway <lisalendway@gmail.com>
2020-03-29 10:14:33 -0500

    updated app with link to raw data

--- shiny_app/app.R
+++ shiny_app/app.R
@@ -9,8 +9,8 @@ covid19_comp_date <- covid19 %>%
   arrange(date) %>% 
   mutate(cases_over20 = cases > 20,
          min_cases_over20 = which.max(cases_over20),
-         days_since_first = row_number() - min_cases_over20
-  ) 
+         days_since_over20 = row_number() - min_cases_over20)
+
 
 state_terr_name <- covid19_comp_date %>% 
   distinct(state) %>% 
@@ -19,7 +19,9 @@ state_terr_name <- covid19_comp_date %>%
 
 ui <- fluidPage(
   sidebarLayout(
-    sidebarPanel(
+    sidebarPanel(p("Choose US States/Territories to compare their # of cases of covid-19."),
+                 p("Find the raw data at:"),
+                 a("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv"),
       selectInput("state", "US State/Territory", 
                   choices = state_terr_name, 
                   multiple = TRUE),
@@ -37,7 +39,7 @@ server <- function(input, output) {
   output$covid_comp <- renderPlot({
     covid_plot <- covid19_comp_date %>% 
       filter(state %in% input$state) %>% 
-      ggplot(aes(x = days_since_first, y = cases, 
+      ggplot(aes(x = days_since_over20, y = cases, 
                  group = state, 
                  color = state)) +
       geom_line() +
